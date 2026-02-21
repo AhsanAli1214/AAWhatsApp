f4d0afda3a7752355e6ddba74e7d355728126c83
Ahsan Ali <55105227-ahsanaliwadani6@users.noreply.replit.com>
2026-02-21 14:01:14 +0000

    #

--- client/src/lib/branding.ts
+++ client/src/lib/branding.ts
@@ -1,3 +1,4 @@
 export const APP_LOGO_URL = "/favicon.png";
 
-export const AA_WHATSAPP_ICON_URL = "/favicon.png";
+export const AA_WHATSAPP_ICON_URL =
+  "https://i.postimg.cc/Kv87k6g3/cropped_circle_image_(5).png";
